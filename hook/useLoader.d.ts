441b789310bef93d932a6a8d0276ae9d229efd7a
SalvatoreLa <salvatore.la.spata@gmail.com>
2022-11-02 19:17:06 +0100

    17

--- hook/useLoader.d.ts
+++
@@ -1,6 +0,0 @@
-import * as THREE from 'three';
-import { BackgroundConfig, ObjectConfig, TextureConfig } from '../types/types';
-export declare const loadModel: (object: ObjectConfig) => Promise<THREE.Group> | Promise<THREE.Object3D<THREE.Event>>;
-export declare const loadBackground: (background?: BackgroundConfig) => Promise<THREE.DataTexture> | Promise<THREE.CanvasTexture>;
-export declare const loadTexture: (texture?: TextureConfig) => Promise<THREE.Texture> | Promise<THREE.CanvasTexture>;
-//# sourceMappingURL=useLoader.d.ts.map
